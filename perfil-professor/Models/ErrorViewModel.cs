64bdba566d726c3728448af9030e3dab6e62d1c8
arthu <arthu@DESKTOP-OB5Q82R>
2024-04-09 09:04:49 -0300

    Adicionar arquivos de projeto.

---
+++ perfil-professor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace perfil_professor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
