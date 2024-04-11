efcac9fed0e405af48d77933e387229ceba35984
arthurviotti <arthur.viotti@hotmail.com>
2024-04-11 11:30:26 -0300

    Configuração do Banco de Dados e da Tabela Professores

---
+++ perfil-professor/Migrations/20240411142024_M01-AddTableProfessores.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace perfil_professor.Migrations
+{
+    public partial class M01AddTableProfessores : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Professores",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mudarsenha = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professores", x => x.Name);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Professores");
+        }
+    }
+}
---
+++ perfil-professor/Models/Professor.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace perfil_professor.Models
+{
+    [Table("Professores")]
+    public class Professor
+    {
+        [Key]
+
+        [Required(ErrorMessage = "Obrigatório informar o nome")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Username")]
+
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Email")]
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Mudarsenha")]
+
+        public string Mudarsenha { get; set; }
+
+
+
+    }
+}
